4d9e8f1def8e46f22b1d1a2d8b5fc106ae28ec4a
Islam Dudaev <islamwolf23@gmail.com>
2019-04-18 01:34:26 +0100

    Remove serviceWorkers

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-         hello
-        </header>
+      <div>
+        Local Service
       </div>
     );
   }
 }
-
-export default App;
