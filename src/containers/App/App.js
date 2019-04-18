16be893ed57dafb6d4c0e4af425c1687edfdc8fc
Islam Dudaev <islamwolf23@gmail.com>
2019-04-18 02:36:52 +0100

    Install material UI

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         Local Service
+        <Button color="primary">
+          Hello World
+        </Button>
       </div>
     );
   }
